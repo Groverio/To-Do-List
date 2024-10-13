98b0d5020a7fcfb49144a4d9cfd08114985b6451
albert-murphy <albertmurphy128@gmail.com>
2024-10-13 18:48:41 +0100

    Fixed a bug where users could enter in a past date and a past time. I implemented two validation checks to ensure date and time were greater then min, if not displayed an alert.

--- script.js
+++ script.js
@@ -39,6 +39,18 @@ function addTodo() {
     return;
   }
 
+  // Check that date is not in past
+  if (date < inputDateElement.min) {
+    alert('Please select the current date or a future date.');
+    return;
+  }
+
+  // Check that time is not in past
+  if (time < inputTimeElement.min) {
+    alert('Please select a future time.');
+    return;
+  }
+
   if (isEditing) {
     // Update the existing todo
     todoList[editIndex] = {
@@ -192,6 +204,7 @@ function setDefaultDateTime() {
   inputDateElement.value = date;
   inputDateElement.min = date; // Set the min attribute to today's date
   inputTimeElement.value = time;
+  inputTimeElement.min = time; // Set the min attribute to current time
 }
 
 function sortTodos(sortBy) {
