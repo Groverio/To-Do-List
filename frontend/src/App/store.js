cfc3e0b69f47c3a7227ad4b8f2085e1c7c57c0a2
Shangesh S <shangesh2006@gmail.com>
2024-10-17 19:17:25 +0530

    Delete frontend directory
    Signed-off-by: Shangesh S <shangesh2006@gmail.com>

--- frontend/src/App/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from './Slices/todoSlice';
-
-export const store = configureStore({
-  reducer: {
-    todos: todoReducer,
-  },
-});
