22a511525a39e5cccd964a1cd85f9b5c6570682d
VarshiniShreeV <varshinishreevelumani@gmail.com>
2024-10-18 15:59:03 +0530

    Sorting by Date Functionality Added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,15 @@ export default [
     files: ['**/*.js'],
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2021, // Use the latest ECMAScript version
+        sourceType: 'module', // Enable ES6 modules
+      },
+    },
+    rules: {
+      'quotes': ['error', 'single'], // Enforce single quotes
+      'semi': ['error', 'always'], // Enforce semicolons
+      // Add more rules as needed
     },
   },
   pluginJs.configs.recommended,
