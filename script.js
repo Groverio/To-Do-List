369419f9cb38a613af187f4180f6c3e1ae57ac14
Cheng Wang <chengwang1009@gmail.com>
2024-10-27 11:15:50 +0800

    npm run lint

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ function updateTodoList() {
       }
     });
 
-    updateTaskCounter();
+  updateTaskCounter();
 
   const addElement = document.querySelector('.js-add-html');
   todoListhtml = '';
